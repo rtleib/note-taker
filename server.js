2a9da85aa97245a7d68746f04ebdb4f398d907af
rtleib <rtleib@gmail.com>
2022-04-24 18:50:38 -0500

    not going with the route method, fixed it by moving it to server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const htmlRoutes = require('./public/assets/js/html');
-// const apiRoutes = require('./public/assets/js/api');
+
+const path = require('path');
 
 // create port varible and initizing the app
 const app = express();
 const PORT = 3001;
-app.use('/', htmlRoutes);
-// app.use('*', apiRoutes);
 
 // set up body parsing and middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Get route for /notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+  
+  // GET route for *
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
 // start the server
 app.listen(PORT, () => {
